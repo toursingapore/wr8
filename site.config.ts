675a2278cf74a1685de67cc5d31906da3e7e8207
toursingapore <158420206+toursingapore@users.noreply.github.com>
2024-03-05 16:25:34 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7167d2cf133a4875b7e23caf545ee402',
+  rootNotionPageId: '9cb8229e7d3d4021a13704bf69ccf61f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
